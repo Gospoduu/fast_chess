0a63912d8174c855a5b9f27fd7612693d85c987e
Артём Терешонок <teresh@MacBook-Pro-Artem.local>
2026-04-29 20:06:38 +0300

    three123

--- src/app/services/Board.py
+++ src/app/services/Board.py
@@ -1,1 +1,23 @@
-print('Hello world!')
+from .Figures import Figures
+
+class Board:
+    @staticmethod
+    def arrangement_of_figures(board): # Расстановка фигур
+        board = [
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            []
+        ]
+        return board
+
+    def __init__(self):
+        self.board = self.arrangement_of_figures()
+
+    def move(self, old_position, new_positions):
+        self.board[new_positions[0]][new_positions[1]] = self.board[old_position[0]][old_position[1]]
+                   
---
+++ src/app/services/Figures.py
@@ -0,0 +1,15 @@
+from Board import Board
+
+class Figures:
+    def __init__(self, color, position):
+        self.color = color
+        self.position = position
+
+
+    def can_move(self, board):
+        pass
+
+
+class Pawn(Figures):
+    def __init__(self, color, position):
+        pass
